20f1265f7523902c3c2e7a31c171a05d706c6620
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-10-04 13:32:32 +0900

    Update Thu.java

--- Sololearn/Coach/Thu.java
+++ Sololearn/Coach/Thu.java
@@ -1,8 +1,11 @@
 public class Thu {
+
     public static void main(String[] args) {
-        int imin=Integer.MIN_VALUE;
-        int imax=Integer.MAX_VALUE;
-        System.out.println(imin);
-        System.out.println(imax);
+           int x=8;
+           double w=7.5;
+           double y=x+w;
+           double z=x+y;
+           System.out.println(y);
+           System.out.println(z);
         }
 }
