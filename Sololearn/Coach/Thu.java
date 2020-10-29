0d51e0c21abef4ffbf4e70ed1e9a57218fbc09a5
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-10-29 18:51:02 +0900

    Update Thu.java

--- Sololearn/Coach/Thu.java
+++ Sololearn/Coach/Thu.java
@@ -1,11 +1,16 @@
 public class Thu {
-
+    private static void tri(){
+        System.out.println("aaa");
+    }
     public static void main(String[] args) {
-           int x=8;
-           double w=7.5;
-           double y=x+w;
-           double z=x+y;
-           System.out.println(y);
-           System.out.println(z);
+        Thu obj=null;
+        obj.tri();
+        Thu.tri();
+        //    int x=8;
+        //    double w=7.5;
+        //    double y=x+w;
+        //    double z=x+y;
+        //    System.out.println(y);
+        //    System.out.println(z);
         }
 }
