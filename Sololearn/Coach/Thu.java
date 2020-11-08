5312c909dcae2154ea5e1b925c1c8ebdcef5e5b5
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-11-08 18:49:34 +0900

    Update Thu.java

--- Sololearn/Coach/Thu.java
+++ Sololearn/Coach/Thu.java
@@ -1,14 +1,20 @@
 public class Thu {
-    static int value=8;
-    static void click(){
-        if (value %4 == 0) value /=2;
-        else value +=3;
+    static int ans(int a,int b){
+        int c=a;
+        System.out.println("First: "+c);
+        for (c=a;c<b;c++){
+            c+=c;
+            System.out.println("i ("+c+")="+c);
+        }
+        return c;
     }
     public static void main(String[] args) {
-        for (int i=0; i<=3;i++){
-            click();
-            System.out.println(value);
-        }
-        // System.out.println(value);
+        System.out.println(Thu.ans(2,6));
+        String s1="Phat";
+        String s2="Ph";
+        String s3=s2+"at";
+        String s4="Phat";
+        System.out.println(s1 == s3); 
+        System.out.println(s1 == s4); 
     }
 }
