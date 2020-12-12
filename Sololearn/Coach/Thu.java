f01bb056bbc5408bf7adeefffa4acf5cc3b02207
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-12-12 17:19:26 +0900

    Update Code

--- Sololearn/Coach/Thu.java
+++ Sololearn/Coach/Thu.java
@@ -4,8 +4,14 @@ class Number{
 public class Thu {
     
     public static void main(String[] args) {
-        int x=0;
-        int y=0;
-        System.out.println("Tong cua x va y la: "+x+y);
+        // int x=0;
+        // int y=0;
+        // System.out.println("Tong cua x va y la: "+x+y);
+        String s="@Dayla @";
+        String[] st=s.split("@");
+        System.out.println(st.length);
+        for (String tt:st){
+            System.out.println("Em :" +tt);
+        }
     }
 }
