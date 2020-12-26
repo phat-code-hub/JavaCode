853592e9a9e0417bcf883e67fc00d07b7452554f
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-12-26 18:47:45 +0900

    Create b1.java

---
+++ Sololearn/Dau/Khac/b1.java
@@ -0,0 +1,12 @@
+package Dau.Khac;
+
+public class b1 {
+    public static void main(String[] args) {
+        Integer i1=127;
+        Integer i2=127;
+        System.out.println(i1 == i2);
+        i1=128;
+        i2=128;
+        System.out.println(i1 == i2);
+    }
+}
