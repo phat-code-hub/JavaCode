3b862d1943e207fd0fb5ec22ba31433a731059ca
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-12-28 17:45:49 +0900

    Update Codes
    Correct number Format

--- Sololearn/Coach/Challenge/LoanCalculator.java
+++ Sololearn/Coach/Challenge/LoanCalculator.java
@@ -7,11 +7,14 @@ public class LoanCalculator {
     static final int MONTHS=6;
     static int calPayment (int amount ){
         double pay=(double) amount;
+        // double paid;
         int month=1;
         do {
-            pay=pay*(1-RATE);
+            // paid =pay*RATE;
+            // pay -=paid;
+            pay -= pay*RATE;
         } while (++month<=MONTHS);
-        return (int)pay ;
+        return (int) pay ;
     }
     public static void main (String[] args) {
         Scanner scanner= new Scanner(System.in);
