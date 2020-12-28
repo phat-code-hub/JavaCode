cc942b372fe46132e8399528fcc1ce1425735aeb
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-12-28 16:34:22 +0900

    New Challenge Codes

---
+++ Sololearn/Coach/Challenge/LoanCalculator.java
@@ -0,0 +1,28 @@
+package Challenge;
+
+import java.util.Scanner;
+
+public class LoanCalculator {
+    static final double RATE=0.1;
+    static final int MONTHS=6;
+    static int calPayment (int amount ){
+        double pay=(double) amount;
+        int month=1;
+        do {
+            pay=pay*(1-RATE);
+        } while (++month<=MONTHS);
+        return (int)pay ;
+    }
+    public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        try {
+            int loan= sc.nextInt();
+            int remain=calPayment(loan);
+            System.out.println(remain);
+        }
+        finally{
+
+        } sc.close();
+
+    }
+}
