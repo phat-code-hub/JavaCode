97fd4aa6a79bce19535408166c359bab15adfa87
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-12-31 14:04:32 +0900

    Update TheSpyLife.java

--- Sololearn/Coach/Challenge/TheSpyLife.java
+++ Sololearn/Coach/Challenge/TheSpyLife.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class TheSpyLife {
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
-        String mess=sc.nextLine().trim();
+        String message=sc.nextLine().trim();
         String spyLife=""; 
-        for (char c:mess.toCharArray()){
-            if(Character.isAlphabetic(c)){
+        for (char c:message.toCharArray()){
+            if(Character.isAlphabetic(c)|| c==' '){
                 spyLife =c +spyLife;
             }
         }
