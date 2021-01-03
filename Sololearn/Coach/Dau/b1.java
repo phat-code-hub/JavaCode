219a63fc54b8a3b0d779494fd92a83ffcb84ec0d
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2021-01-03 18:19:36 +0900

    Update b1.java

--- Sololearn/Coach/Dau/b1.java
+++ Sololearn/Coach/Dau/b1.java
@@ -2,13 +2,7 @@ package Dau;
 
 public class b1 {
     public static void main(String[] args){
-        String x="5a";
-        int y=0;
-        try {
-            y=Integer.parseInt(x)%2;
-            System.out.println(y);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        Error err= new Error();
+        System.out.println(err);
     }
 }
