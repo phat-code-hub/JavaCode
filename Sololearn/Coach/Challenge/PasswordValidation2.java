6720124780126d9178792c153e02fa23f4f6ce0a
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2021-01-04 18:44:33 +0900

    Update PasswordValidation2.java

--- Sololearn/Coach/Challenge/PasswordValidation2.java
+++ Sololearn/Coach/Challenge/PasswordValidation2.java
@@ -3,7 +3,7 @@ package Challenge;
 import java.util.Scanner;
 
 public class PasswordValidation2 {
-    static boolean checkPassword(final String word){
+    static String checkPassword(final String word){
         final String SpecialWords="!@#$%&*";
         final char[] chars= word.toCharArray();
         int sum1=0,sum2=0;
@@ -11,7 +11,7 @@ public class PasswordValidation2 {
             if (SpecialWords.contains(Character.toString(c))) sum1++;
             if(Character.isDigit(c)) sum2++;
         }
-        return  word.trim().length()>=7 && sum1 >=2 && sum2 >=2 ;
+        return  (word.trim().length()>=7 && sum1 >=2 && sum2 >=2)==true? "Strong":"Weak" ;
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
