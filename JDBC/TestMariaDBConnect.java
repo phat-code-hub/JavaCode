87b55bb1f97e030adcd5cfa29070aa46cd2d3d69
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2021-03-26 21:47:20 +0900

    Change Database

--- JDBC/TestMariaDBConnect.java
+++ JDBC/TestMariaDBConnect.java
@@ -4,8 +4,8 @@ import java.sql.*;
 
 public class TestMariaDBConnect {
     public static void main(String[] args) throws SQLException {
-        String sql = "Select * From Phat";
-        try (Connection con = ConnectMariaDB.getConnect("JavaDB");
+        String sql = "Select * From gold";
+        try (Connection con = ConnectMariaDB.getConnect("GoldSE11");
                 Statement stmt = con.createStatement();
                 ResultSet rs = stmt.executeQuery(sql);) {
             while (rs.next()) {
