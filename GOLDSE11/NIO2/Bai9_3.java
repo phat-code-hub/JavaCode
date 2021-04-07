b88317c60f4b9fa4c97ff8a50c81e45402a4bbb9
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2021-04-07 22:08:43 +0900

    New Codes

---
+++ GOLDSE11/NIO2/Bai9_3.java
@@ -0,0 +1,10 @@
+package GOLDSE11.NIO2;
+
+import java.io.Console;
+
+public class Bai9_3 {
+    public static void main(String[] args) {
+        Console con = System.console();
+        con.printf(con.readLine("Nhap tu di: "));
+    }
+}
---
+++ GOLDSE11/Thread/Bai8_4.java
@@ -0,0 +1,16 @@
+package GOLDSE11.Thread;
+
+public class Bai8_4 {
+    public static void main(String[] args) {
+        Runnable r = () -> System.out.println("Run... ");
+        Thread t = new Thread(r);
+        t.start();
+        try {
+            t.join(); // make main thread wait until this t thread finish
+            System.out.println("main ");
+        } catch (InterruptedException e) {
+            // e.printStackTrace();
+            System.err.println("Exception!");
+        }
+    }
+}
