68f754e8aea9d3f6cd112583638c66fdfc1ed0a7
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2021-04-16 23:12:38 +0900

    Create Bai8_5.java

---
+++ GOLDSE11/Thread/Bai8_5.java
@@ -0,0 +1,27 @@
+package GOLDSE11.Thread;
+
+public class Bai8_5 extends Thread {
+    public void run() {
+        try {
+            while (true) {
+                System.out.print("*");
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            // e.printStackTrace();
+            System.out.println("Interrupt");
+        }
+    }
+
+    public static void main(String[] args) {
+        Bai8_5 t = new Bai8_5();
+        t.start();
+        try {
+            int st = 3000 + (int) (Math.random() * 10000);
+            Thread.sleep(st);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t.interrupt();
+    }
+}
