7ace6dc88290faece9adcba6bb9fc5e94153a21f
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2021-05-15 21:27:32 +0900

    Update Bai5_3RegMatcher.java

--- SachJava/Ch5StandardLibrary/Bai5_3RegMatcher.java
+++ SachJava/Ch5StandardLibrary/Bai5_3RegMatcher.java
@@ -4,11 +4,17 @@ import java.util.regex.*;
 
 public class Bai5_3RegMatcher {
     public static void main(String[] args) {
-        var msg = "会社は0123-99-0000です。自宅は000-123-4567ですよ！";
-        var ptn = Pattern.compile("(?<area>\\d{2,4})-(?<city>\\d{2,4})-(?<local>\\d{4})");
-        var mat = ptn.matcher(msg);
+        String msg = "会社は0123-99-0000です。自宅は000-123-4567ですよ！";
+        Pattern ptn = Pattern.compile("(?<area>\\d{2,4})-(?<city>\\d{2,4})-(?<local>\\d{4})");
+        Matcher mat = ptn.matcher(msg);
         while (mat.find()) {
-            System.out.println("マーチング文字列：　" + mat.group());
+            System.out.println("開始位置: " + mat.start());
+            System.out.println("終了位置: " + mat.end());
+            System.out.println("マーチング位置: " + mat.group());
+            System.out.println("市街局番: " + mat.group("area"));
+            System.out.println("市内局番: " + mat.group("city"));
+            System.out.println("加入者番号: " + mat.group("local"));
+            System.out.println("------------------------------");
         }
     }
 }
