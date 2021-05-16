5017295bc20510d11e353ce373a931161f860644
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2021-05-16 14:01:25 +0900

    Create b2_Modulus.java

---
+++ Sololearn/ThiDau/b2_Modulus.java
@@ -0,0 +1,9 @@
+package ThiDau;
+
+public class b2_Modulus {
+    public static void main(String[] args) {
+        double a=-5.5;
+        double b=1.5;
+        System.out.println(a % b);
+    }
+}
