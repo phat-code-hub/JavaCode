c9ece2a250e76e3f2b51b03400ad71ae91f79c26
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2021-05-21 18:57:45 +0900

    Create b3Variable.java

---
+++ Sololearn/ThiDau/b3Variable.java
@@ -0,0 +1,12 @@
+package ThiDau;
+
+public class b3Variable {
+    public static void main(String[] args) {
+        int ss=fun(2);
+        // System.out.println(fun(8)); //OK
+        System.out.println(ss);
+    }
+    static int fun(int x ) {
+        return x;
+    }
+}
